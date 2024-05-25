4d432f0c3041c41270add780da03c603b06cfb6c
Eduardo Fernandez <edward.jfer@outlook.com>
2024-05-25 16:00:12 -0400

    minor corrections

--- front-end/src/components/main-chat-container.tsx
+++ front-end/src/components/main-chat-container.tsx
@@ -1,5 +1,9 @@
 function MainChatContainer() {
-  return <div className="w-full"></div>;
+  return (
+    <div className="w-full">
+      <></>
+    </div>
+  );
 }
 
 export default MainChatContainer;
